a9ecae16d8a995222c79624af3699ccd51a773af
Gipnokot <36129002+Gipnokot@users.noreply.github.com>
2025-04-24 19:12:41 +0500

    Update hash_create.rb

--- hash_create.rb
+++ hash_create.rb
@@ -1,7 +1,6 @@
-user = { name: "Almaz", age: 31, city: "Ufa" }
+user = { name: "John", surname: "Doe", age: 30, city: "Moscow" }
 
 puts user[:name]
+puts user[:surname]
 puts user[:age]
 puts user[:city]
-
-#)))))))))))
